1f3ecaaab6152dfd9336183f409171e955c7e352
bagus-ilham <itkantiss@gmail.com>
2026-06-23 14:17:15 +0700

    fix CORS origin wildcard

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,10 +1,8 @@
 // Shared CORS headers for all Edge Functions
 declare const Deno: any;
 
-const allowedOrigin = Deno.env.get("APP_URL") || "*";
-
 export const corsHeaders = {
-  "Access-Control-Allow-Origin": allowedOrigin,
+  "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers":
     "authorization, x-client-info, apikey, content-type",
   "Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, DELETE",
